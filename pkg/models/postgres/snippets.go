e8a8a38b0dbad780fbb8707b414654192afcd9c8
Creata21 <yerezhepabdulla@gmail.com>
2023-06-09 18:56:57 +0600

    feat: changed db to postgres instead of mysql

--- pkg/models/postgres/snippets.go
+++ pkg/models/postgres/snippets.go
@@ -1,4 +1,4 @@
-package mysql
+package postgres
 
 import (
 	"Creata21/snippetbox/pkg/models"
@@ -11,29 +11,26 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	query := `INSERT INTO snippets (title, content, created, expires) VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+	query := `INSERT INTO snippets (title, content, created) VALUES ($1, $2, CURRENT_TIMESTAMP)`
 
-	result, err := m.DB.Exec(query, title, content, expires)
-
-	if err != nil {
-		return 0, err
-	}
-
-	id, err := result.LastInsertId()
+	var id int
+	err := m.DB.QueryRow(query, title, content).Scan(&id)
 
 	if err != nil {
 		return 0, err
 	}
+
 	fmt.Println(id)
-	return int(id), nil
+	return id, nil
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	query := `SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id = ?`
+	query := `SELECT id, title, content, created FROM snippets WHERE id = $1`
+
 	row := m.DB.QueryRow(query, id)
 	s := &models.Snippet{}
 
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created)
 
 	if err == sql.ErrNoRows {
 		return nil, models.ErrNoRecord
@@ -45,23 +42,25 @@ func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 }
 
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
-	query := `SELECT * FROM snippets WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
+	query := `SELECT * FROM snippets ORDER BY created DESC LIMIT 10`
 
 	rows, err := m.DB.Query(query)
+
 	if err != nil {
 		return nil, err
 	}
+
 	defer rows.Close()
 	snippets := []*models.Snippet{}
-	
+
 	for rows.Next() {
 		s := &models.Snippet{}
-		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created)
 
 		if err != nil {
 			return nil, err
 		}
-		
+
 		snippets = append(snippets, s)
 	}
 
